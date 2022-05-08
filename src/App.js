3e8683fc8d1537fc67ca3445ca7b881b9f055dc2
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-08 13:06:07 -0500

    Agregando editarUsuario

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 import UserTable from './components/UserTable';
+import AddUserForm from './components/AddUserForm';
+import EditUserForm from './components/EditUserForm';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
@@ -22,20 +24,67 @@ function App() {
     ])
   }
 
+  //Eliminar un usuario
+  const deleteUser = (id) => {
+    
+    const arrayFiltrado = users.filter(user => user.id !== id);
+    setUsers(arrayFiltrado);
+
+  }
+
+  //editar usuario
+  const [editing, setEditing] = useState(false);
+
+  const [curretUser, setCurrentUser] = useState({
+    id: null, name: '', username:''
+  });
+
+  const editRow = (user) => {
+    setEditing(true);
+    setCurrentUser({
+      id: user.id, name: user.name, username: user.username
+    })
+  }
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false);
+    setUsers(users.map(user =>(user.id === id ? updatedUser : user)))
+  }
+
   return (
    <div className='container'>
-     <h1>CRUPD App with Hooks</h1>
+     <h1>Crud App with Hooks</h1>
      <div className='flex-row'>
-       <div className='flex-large'>
-         <h2>Add user</h2>
-       </div>
-       <div className='flex-large'>
-         <UserTable users={users}/>
-       </div>
-     </div>
+      <div className='flex-large'>
+        
+        {
+          editing ? (
+            <div>
+              <h2>Edit user</h2>
+              <EditUserForm 
+              curretUser={curretUser}
+              updateUser={updateUser}
+              />
+            </div>
+          ) : <div>
+                <h2>Add user</h2>
+                <AddUserForm addUser={addUser}/>
+            </div>
+        }
 
+        
+        
+      </div>
+      <div className='flex-large'>
+        <h2>View users</h2>
+        <UserTable users={users} 
+        deleteUser={deleteUser} 
+        editRow={editRow}
+        />
+      </div>
+     </div>
    </div>
   );
 }
 
-export default App;
+export default App
