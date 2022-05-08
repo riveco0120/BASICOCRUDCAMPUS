ac7c309ea4c55accd866b9481e900b38c332bd3e
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-08 13:37:40 -0500

    Agregando compoentes finales
    La aplicacion funciona

--- src/components/AddUserForm.jsx
+++ src/components/AddUserForm.jsx
@@ -1,38 +1,42 @@
-const {register, errors, handleSubmit} = useForm();
+import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const AddUserForm = (props) => {
-const onSubmit = (data, e) => {
-    //console.log(data)
+    
+    const {register, errors, handleSubmit} = useForm();
 
-    props.addUser(data)
+    const onSubmit = (data, e) => {
+        //console.log(data)
 
-    //limpiar campos
-    e.target.reset();
-}
+        props.addUser(data)
 
-return(
-    <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Name</label>
-        <input type="text" name="name" {
-            ...register(
-                'name', {required: true, message: "campo Requerido"}
-            )
-        }/>
-        <div>
-            {errors?.name?.message}
-        </div>
-        <label>Username</label>
-        <input type="text" name="username" {
-            ...register(
-                'username', {required: true, message: "ampo Requerido"}
-            )
-        }/>
-        <div>
-            {errors?.username?.message}
-        </div>
-        <button>Add new user</button>
-    </form>
-);
+        //limpiar campos
+        e.target.reset();
+    }
+    
+    return(
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>Name</label>
+            <input type="text" name="name" {
+                ...register(
+                    'name', {required: true, message: "campo Requerido"}
+                )
+            }/>
+            <div>
+                {errors?.name?.message}
+            </div>
+            <label>Username</label>
+            <input type="text" name="username" {
+                ...register(
+                    'username', {required: true, message: "ampo Requerido"}
+                )
+            }/>
+            <div>
+                {errors?.username?.message}
+            </div>
+            <button>Add new user</button>
+        </form>
+    );
 }
 
 export default AddUserForm;
--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
 
-const UserTable = ({ props}) => {
-    return (<table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>UserName</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>{props.users.length > 0 ?
+const UserTable = (props) => {
+
+    console.log(props.users)
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>UserName</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>{
+                //mostrar itelaracion usuarios 
+                    props.users.length > 0 ?
                     props.users.map(user => (
                         <tr key={user.id}>
                             <td> {user.name} </td>
                             <td> {user.username} </td>
                             <td>
-                                <button className='button muted-button'>Edit</button>
-                                <button className='button muted-button'>Delete</button>
+                            <button className='button muted-button'
+                                onClick={
+                                    () => {props.editRow(user)}
+                                }
+                            >Edit</button>
+                            <button 
+                                className='button muted-button'
+                                onClick={() => {props.deleteUser(user.id)}}
+                            >
+                                    Delete
+                            </button>
                             </td>
                         </tr>
                     )) : (
@@ -25,9 +39,10 @@ const UserTable = ({ props}) => {
                         </tr>
                     )
                 }
-             </tbody>
-    </table>
+                
+            </tbody>
+        </table>
     );
 }
-
+ 
 export default UserTable;
